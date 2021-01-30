16d9ad5ea3f113f09e3753ea3e488cf261b08785
Oliver Otchere <oliverotchere4@gmail.com>
2021-01-30 21:07:05 +0000

    added home views

--- weather/views.py
+++ weather/views.py
@@ -4,32 +4,29 @@ import requests
 import os
 
 from .forms import searchForm
+from .helpers import getLocationDet,getTemperatureDet
 
 
 def home(request):
     form=searchForm(request.POST or None)
 
-    apikey=os.getenv('ACCUWEATHER_APIKEY')
-
-    respone=requests.get(f'http://dataservice.accuweather.com/locations/v1/cities/search?apikey={apikey}&q=accra',)
-
-    result=respone.json()[0]
-
-    name,locKey=result['EnglishName'],result['Key']
-
-    temp_response=requests.get(f'http://dataservice.accuweather.com/currentconditions/v1/{locKey}?apikey={apikey}')
-
-    temp_result=temp_response.json()
-
-    context={
-        'city_name':name,
-        'is_day':temp_result[0]['IsDayTime'],
-        'temperature':temp_result[0]['Temperature']['Metric']['Value']
-    }
+    context={}
 
     if form.is_valid():
-        print('yes',request.POST.get('location'))
-    else:
-        print('no')
+        locationDet=getLocationDet(request.POST.get('location'))
+
+        if locationDet:
+
+            name,locKey=locationDet['EnglishName'],locationDet['Key']
+
+            temperatureDet=getTemperatureDet(locKey)
+
+            context={
+                'city_name':name,
+                'is_day':temperatureDet[0]['IsDayTime'],
+                'temperature':temperatureDet[0]['Temperature']['Metric']['Value']
+            }
+
+    
 
     return render(request,'weather_app/index.html',context)
