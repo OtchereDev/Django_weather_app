2ffa425e625ea4a7d79cb751f1dac735da9ffa30
Oliver Otchere <oliverotchere4@gmail.com>
2021-01-31 23:32:01 +0000

    refactored view with home view having default city

--- weather/views.py
+++ weather/views.py
@@ -4,30 +4,16 @@ import requests
 import os
 
 from .forms import searchForm
-from .helpers import getLocationDet,getTemperatureDet,getName_LocKey,getForecastDet
+from .helpers import getLocationDet,getTemperatureDet,getName_LocKey,getForecastDet,getForecastWeather,getCurrentWeather
 
 
 def home(request):
     form=searchForm(request.POST or None)
 
-    context={}
+    context=getCurrentWeather(request)
 
     if form.is_valid():
-        locationDet=getLocationDet(request.POST.get('location'))
-
-        if locationDet:
-
-            name,locKey=getName_LocKey(locationDet)
-
-            temperatureDet=getTemperatureDet(locKey)
-
-            context={
-                'city_name':name,
-                'is_day':temperatureDet[0]['IsDayTime'],
-                'temperature':temperatureDet[0]['Temperature']['Metric']['Value']
-            }
-
-    
+        context=getCurrentWeather(request)
 
     return render(request,'weather_app/index.html',context)
 
@@ -35,37 +21,9 @@ def home(request):
 def forecast(request):
     form =searchForm(request.POST or None)
 
-    # context={}
-
-    city=request.POST.get('location') or 'accra'
-
-    print(city)
-
-    name,locKey=getName_LocKey( getLocationDet(city))
-        
-    if locKey:
-        currTemp=getTemperatureDet(locKey)
-        forecastTemp=getForecastDet(locKey)
-        context={
-            'city_name':name,
-            'is_day':currTemp[0]['IsDayTime'],
-            'temperature':currTemp[0]['Temperature']['Metric']['Value'],
-            'forecast_detail':forecastTemp[1:],
-        }
+    context=getForecastWeather(request)
 
     if form.is_valid():
-        name,locKey=getName_LocKey( getLocationDet(city))
-        
-        if locKey:
-            currTemp=getTemperatureDet(locKey)
-            forecastTemp=getForecastDet(locKey)
-            context={
-                'city_name':name,
-                'is_day':currTemp[0]['IsDayTime'],
-                'temperature':currTemp[0]['Temperature']['Metric']['Value'],
-                'forecast_detail':forecastTemp[1:],
-            }
-
-            print(context)
+        context=getForecastWeather(request)
 
     return render(request,'weather_app/forecast.html',context)
